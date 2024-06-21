3b19a6e554efc491ee93e0828912b37c445f86d3
github-action-benchmark <github@users.noreply.github.com>
2024-06-21 13:06:23 +0000

    add Benchmark (go) benchmark result for 2ea628907eebc9e92fe34234c61c3696f69c37e1

---
+++ benchmark/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718975183137,
+  "repoUrl": "https://github.com/maladroitthief/mosaic",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "maladroitthief",
+            "username": "maladroitthief"
+          },
+          "committer": {
+            "name": "maladroitthief",
+            "username": "maladroitthief"
+          },
+          "id": "2ea628907eebc9e92fe34234c61c3696f69c37e1",
+          "message": "feat: Circles and Transformations",
+          "timestamp": "2024-06-16T14:32:03Z",
+          "url": "https://github.com/maladroitthief/mosaic/pull/1/commits/2ea628907eebc9e92fe34234c61c3696f69c37e1"
+        },
+        "date": 1718975182220,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPolygonClip",
+            "value": 4367,
+            "unit": "ns/op\t    7408 B/op\t      27 allocs/op",
+            "extra": "271652 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPolygonClip - ns/op",
+            "value": 4367,
+            "unit": "ns/op",
+            "extra": "271652 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPolygonClip - B/op",
+            "value": 7408,
+            "unit": "B/op",
+            "extra": "271652 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPolygonClip - allocs/op",
+            "value": 27,
+            "unit": "allocs/op",
+            "extra": "271652 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
